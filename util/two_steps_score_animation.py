fdb885cd31357a616e1b154834cc7df12160ddf7
listenzcc <listenzcc@mail.bnu.edu.cn>
2024-04-27 20:19:19 +0800

    Develop cat-leaves-submarine animation

--- util/two_steps_score_animation.py
+++ util/two_steps_score_animation.py
@@ -45,8 +45,13 @@ class TwoStepScorer(object):
     std_threshold = 50  # g
 
     state = '1st'
-    score_1st_step = 0
-    score_2nd_step = 0
+
+    # DO NOT control the score_1st, since it is the mean value
+    score_1st = 0
+
+    # Control the score_2nd, it refers the standard value
+    score_2nd = 0
+    score_2nd_range = (0, 100)
 
     def __init__(self):
         self.reset_scores()
@@ -54,21 +59,21 @@ class TwoStepScorer(object):
 
     def reset_scores(self):
         self.state = '1st'
-        self.score_1st_step = 0
-        self.score_2nd_step = 0
+        self.score_1st = 0
+        self.score_2nd = 0
         logger.debug('Reset the TwoStepScorer')
 
     def get_current_state(self):
         return dict(
             state=self.state,
-            score_1st=self.score_1st_step,
-            score_2nd=self.score_2nd_step,
+            score_1st=self.score_1st,
+            score_2nd=self.score_2nd,
         )
 
     def _limit_scores(self):
         '''Keep the scores inside the range of [0, 100]'''
-        # self.score_1st_step = min(100, max(0, self.score_1st_step))
-        self.score_2nd_step = min(100, max(0, self.score_2nd_step))
+        # self.score_1st = min(100, max(0, self.score_1st))
+        self.score_2nd = min(100, max(0, self.score_2nd))
 
     def _update_score(self, data: Any):
         if data is None:
@@ -88,14 +93,14 @@ class TwoStepScorer(object):
         if self.state == '1st':
             # Update the score
             score = mean - self.ref_value
-            self.score_1st_step = score
+            self.score_1st = score
             self._limit_scores()
 
             # The mean value meets the threshold, enter into the 2nd step
             diff = np.abs(score)
             if diff < self.mean_threshold:
                 self.state = '2nd'
-                self.score_2nd_step = 0
+                self.score_2nd = 0
                 logger.debug(f'Lvl up to the 2nd step')
 
             return self.get_current_state()
@@ -112,25 +117,25 @@ class TwoStepScorer(object):
             if diff > self.mean_threshold:
                 # Update rule:
                 score = mean - self.ref_value
-                self.score_1st_step = score
+                self.score_1st = score
                 # Reset the 2nd step score to 0
-                self.score_2nd_step = 0
+                self.score_2nd = 0
                 self._limit_scores()
                 self.state = '1st'
 
                 logger.debug(
-                    f'Lvl down to the 1st step, 1st step score: {self.score_1st_step}')
+                    f'Lvl down to the 1st step, 1st step score: {self.score_1st}')
 
             # --------------------
             # if the mean value is under the threshold,
             # update the score using std
             else:
                 if std < self.std_threshold:
-                    self.score_2nd_step += 10
+                    self.score_2nd += 10
                 else:
-                    self.score_2nd_step -= 10
+                    self.score_2nd -= 10
                 self._limit_scores()
-                logger.debug(f'Updated 2nd step score: {self.score_2nd_step}')
+                logger.debug(f'Updated 2nd step score: {self.score_2nd}')
 
             return self.get_current_state()
 
@@ -139,7 +144,6 @@ class TwoStepScorer(object):
 
 
 class TwoStepScore_Animation_CatLeavesSubmarine(TwoStepScorer, AutomaticAnimation):
-    image_1st = None  # RGBA PIL image
     images_2nd = []  # RBG PIL images
 
     def __init__(self):
@@ -154,27 +158,43 @@ class TwoStepScore_Animation_CatLeavesSubmarine(TwoStepScorer, AutomaticAnimatio
         name = 'cat-leaves-submarine'
         folder = root_path.joinpath(f'img/{name}')
 
-        def _change_img_1(img):
-            return img.convert('RGB').resize((self.width, self.height))
-
-        def _change_img_2(img):
-            return img.convert('RGBA').resize((self.width//4, self.height//4))
+        # --------------------
+        # ! The relative small size of the image
+        # ! The aim is to minimize the working load of the imaging
+        image_size = (self.width, self.height)
 
         # --------------------
         # Load frames
         n = 60
         # n = 6
-        for j in tqdm(range(1, 1+n), f'Loading resources: {name}'):
-            img = _change_img_1(Image.open(folder.joinpath(f'frames/{j}.jpg')))
+        self.images_2nd = []
+        for j in tqdm(range(n), f'Loading frames: {name}'):
+            img = Image.open(folder.joinpath(f'frames/{j+1}.jpg'))
             self.images_2nd.append(img)
         logger.debug(f'Loaded {n} frames of {name}')
 
         # --------------------
-        # Load parts
-        submarine_image = _change_img_2(
-            Image.open(folder.joinpath('parts/潜水艇.png')))
+        # Load the ocean image
+        ocean_image = Image.open(folder.joinpath('parts/ocean-only.jpg'))
+        logger.debug('Loaded ocean image')
+
+        # --------------------
+        # Load submarine image and generate its mask
+        submarine_image = Image.open(
+            folder.joinpath('parts/submarine-only.jpg'))
+        logger.debug('Loaded submarine image')
+        mat = np.sum(np.array(submarine_image), 2)
+        mat[mat == 255] = 0
+        mat[mat > 0] = 255
+        submarine_mask = Image.fromarray(mat, mode='L')
+        logger.debug('Generated submarine image')
+
+        # --------------------
+        self.images_2nd
+        self.image_size = image_size
+        self.ocean_image = ocean_image
         self.submarine_image = submarine_image
-        logger.debug(f'Loaded part of submarine: {submarine_image}')
+        self.submarine_mask = submarine_mask
 
     def update_score(self, data: Any = None):
         state_before = self.get_current_state()
@@ -191,6 +211,11 @@ class TwoStepScore_Animation_CatLeavesSubmarine(TwoStepScorer, AutomaticAnimatio
 
         n_frames = 10
 
+        image_size = (self.width, self.height)
+
+        # --------------------
+        # The 2nd state
+        # Only choose the frames to display
         if state_after['state'] == '2nd':
             score1 = state_before['score_2nd']
             score2 = state_after['score_2nd']
@@ -198,71 +223,77 @@ class TwoStepScore_Animation_CatLeavesSubmarine(TwoStepScorer, AutomaticAnimatio
 
             n = len(self.images_2nd)-1
 
-            for s in [score1] if diff == 0 else np.arange(score1, score2+diff/n_frames/2, diff/n_frames):
-                idx = int(n * s / 100)
-                img = self.images_2nd[idx].resize((self.width, self.height))
+            # Handle the both conditions of diff == 0 and diff != 0
+            for score in [score1] if diff == 0 else np.arange(score1, score2+diff/n_frames/2, diff/n_frames):
+                idx = int(n * score / 100)
+                img = self.images_2nd[idx].resize(image_size)
 
-                draw = ImageDraw.Draw(img, mode='RGB')
-
-                # --------------------
-                r = 0.04
-                x = 0.5
-                y = 0.8
-                draw.rectangle((
-                    self.scale((x-r, y-r)), self.scale((x+r, y+r))
-                ), fill='#00aaaa')
+                # # --------------------
+                # draw = ImageDraw.Draw(img, mode='RGB')
+                # r = 0.04
+                # x = 0.5
+                # y = 0.8
+                # draw.rectangle((
+                #     self.scale((x-r, y-r)), self.scale((x+r, y+r))
+                # ), fill='#00aaaa')
 
                 self.fifo_buffer.append(img)
 
+        # --------------------
+        # The 1st state
+        # Put the submarine on the correct position
         if state_after['state'] == '1st':
             score1 = state_before['score_1st']
             score2 = state_after['score_1st']
+
+            # At least make the score2 of the same sign with the score1,
+            # it prevents the submarine up and down
+            if score1 * score2 < 0:
+                score2 *= -1
+
             diff = score2 - score1
 
-            for s in np.arange(score1, score2+diff/n_frames/2, diff/n_frames):
-                # img = Image.new(mode='RGB', size=(self.width, self.height))
-                img = self.images_2nd[0].resize((self.width, self.height))
+            score_scale = 100  # g
+            _, height = self.submarine_image.size
+            max_d_height = height * 0.2
 
-                draw = ImageDraw.Draw(img, mode='RGB')
-
-                # --------------------
-                r = 0.05
-                x = 0.5
-                y = 0.8
-                draw.rectangle((
-                    self.scale((x-r, y-r)), self.scale((x+r, y+r))
-                ), outline='#aaaaaa')
-
-                # --------------------
-                r = 0.04
-                x = 0.5 + 0.5 * score2 / 200
-                y = 0.8
-                draw.rectangle((
-                    self.scale((x-r, y-r)), self.scale((x+r, y+r))
-                ), outline='#aa0000')
-
-                # --------------------
-                r = 0.04
-                x = 0.5 + 0.5 * s / 200
-                y = 0.8
-                draw.rectangle((
-                    self.scale((x-r, y-r)), self.scale((x+r, y+r))
-                ), fill='#aa0000')
-
-                # --------------------
-                img2 = Image.new(mode='RGB', size=(200, 200))
-                img2 = self.submarine_image.resize((100, 100))
-                x = 0.5 + 0.5 * s / 200
-                y = 0.8
+            for score in np.arange(score1, score2+diff/n_frames/2, diff/n_frames):
+                img = self.ocean_image
+                # score -> infinity, dy -> 1
+                # score -> 0, dy -> 0
+                dy = (1 - np.exp(-np.abs(score / score_scale)))
                 img.paste(
-                    img2,
-                    (int(x*self.width)-50, int(y*self.height)-50),
-                    img2)
+                    self.submarine_image,
+                    (0, int(dy * max_d_height)),
+                    self.submarine_mask)
+                img = img.resize(image_size)
+
+                # # --------------------
+                # draw = ImageDraw.Draw(img, mode='RGB')
+                # r = 0.05
+                # x = 0.5
+                # y = 0.8
+                # draw.rectangle((
+                #     self.scale((x-r, y-r)), self.scale((x+r, y+r))
+                # ), outline='#aaaaaa')
+
+                # # --------------------
+                # r = 0.04
+                # x = 0.5 + 0.5 * score2 / 200
+                # y = 0.8
+                # draw.rectangle((
+                #     self.scale((x-r, y-r)), self.scale((x+r, y+r))
+                # ), outline='#aa0000')
+
+                # # --------------------
+                # r = 0.04
+                # x = 0.5 + 0.5 * score / 200
+                # y = 0.8
+                # draw.rectangle((
+                #     self.scale((x-r, y-r)), self.scale((x+r, y+r))
+                # ), fill='#aa0000')
 
                 self.fifo_buffer.append(img)
-
-        # for img in self.images_2nd:
-        #     self.fifo_buffer.append(img.resize((self.width, self.height)))
 
         Thread(target=self.animating_loop, daemon=True).start()
 
